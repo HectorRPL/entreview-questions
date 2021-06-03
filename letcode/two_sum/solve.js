62a924ed1de6df302f1ae5ed690fc2c0628a61d8
hectorfv <hector.kaizen@gmail.com>
2021-06-03 07:50:01 -0500

    improves

--- letcode/two_sum/solve.js
+++ letcode/two_sum/solve.js
@@ -1,22 +1,28 @@
 // ( [ 2, 7, 11, 15 ], 9 ) => [0, 1]
 
-function twoSum(nums, target) {
-    const complement = new Map(); /*?*/
-    const length = nums.length; /*?*/
+function twoSum(numbersArray, target) {
+    const complement = new Map(); // {} ... { '16': 0 } ... { '11': 1, '16': 0 }
+    const length = numbersArray.length; // Always put the length outside of for(i). Increases the complexity
 
     for (let i = 0; i < length; i++) {
-        if (complement[nums[i]] >= 0 ) {
-            const something = complement[nums[i]]; /*?*/
-            return [something, i]; /*?*/
+        // console.log('complement ', complement);
+        // console.log('nums[i] ', numbersArray[i]);
+        // console.log('complement[numbersArray[i]] ', complement[numbersArray[i]]);
+        // console.log(complement[numbersArray[i]] >= 0);
+        if (complement[numbersArray[i]] >= 0) {
+            const something = complement[numbersArray[i]]; // {0:}
+            return [something, i];
         }
-        const what = [target - nums[i]]; /*?*/
-        complement[what] = i; /*?*/
+        const what = [target - numbersArray[i]]; // => ???
+        complement[what] = i;
+        console.log(what);
+        // console.log('=> ', complement);
     }
 
     return [];
 }
 
-console.log(twoSum([2, 7, 11, 15], 18));
+console.log(twoSum([2, 7, 11, 15], 26));
 
 
 
---
+++ letcode/two_sum/try-1.js
@@ -0,0 +1,18 @@
+const arrOne = [2, 7, 11, 15];
+const target = 9;
+const getTwoNumThatTheySumTheTarget = (arr, target) => {
+    let nums;
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            if ((arr[i] + arr[j]) === target) {
+                console.log(`${arr[i]} + ${arr[j]} = `, arr[i] + arr[j]);
+                nums = [arr[i], arr[j]];
+                break;
+            }
+        }
+        if (nums === target) return;
+    }
+    return nums;
+};
+
+console.log(getTwoNumThatTheySumTheTarget(arrOne, target));
