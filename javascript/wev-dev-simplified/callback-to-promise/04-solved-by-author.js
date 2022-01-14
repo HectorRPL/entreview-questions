a3c1bbc1cad2747db348675c1703e18a124ee1ca
HFV <hector.kaizen@gmail.com>
2022-01-14 13:19:16 -0600

    add study

---
+++ javascript/wev-dev-simplified/callback-to-promise/04-solved-by-author.js
@@ -0,0 +1,19 @@
+
+
+const task01 = new Promise(resolve => resolve('H'))
+const task02 = new Promise(resolve => resolve('E'))
+const task03 = new Promise(resolve => resolve('L'))
+const task04 = new Promise(resolve => resolve('L'))
+const task05 = new Promise(resolve => resolve('O'))
+
+
+Promise.all([
+  task01,
+  task02,
+  task03,
+  task04,
+  task05,
+])
+  .then(value => console.log(value))
+  .catch()
+
