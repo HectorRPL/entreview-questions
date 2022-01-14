34110ed8cf0b65f37d895d14dd561193d18f3790
HFV <hector.kaizen@gmail.com>
2022-01-14 13:27:25 -0600

    learning

--- javascript/wev-dev-simplified/callback-to-promise/04-my-try.js
+++ javascript/wev-dev-simplified/callback-to-promise/04-my-try.js
@@ -1,10 +1,10 @@
 
 
-const task01 = () => new Promise(resolve => resolve('H'))
-const task02 = () => new Promise(resolve => resolve('E'))
-const task03 = () => new Promise(resolve => resolve('L'))
+const task01 = () => new Promise(resolve => resolve('W'))
+const task02 = () => new Promise(resolve => resolve('O'))
+const task03 = () => new Promise(resolve => resolve('R'))
 const task04 = () => new Promise(resolve => resolve('L'))
-const task05 = () => new Promise(resolve => resolve('O'))
+const task05 = () => new Promise(resolve => resolve('D'))
 
 
 const sayHello = () => {
@@ -18,3 +18,40 @@ const sayHello = () => {
 }
 
 sayHello()
+
+/*
+
+//// LEARNING 01:
+
+This is bad:
+
+const p1 = () => new Promise(r => r('H')) // this declaration is incorrect
+
+This is correct
+const p1 = new Promise(r => r('H'))
+
+//// LEARNING 02:
+
+This is bad:
+
+new Promise.all([ // the key wor 'new' is invalid, you need to assign in a variable!
+    task01,
+    task02,
+    task03,
+    task04,
+    task05,
+  ])
+
+This is correct:
+
+Promise.all([
+  task01,
+  task02,
+  task03,
+  task04,
+  task05,
+])
+
+
+
+* */
