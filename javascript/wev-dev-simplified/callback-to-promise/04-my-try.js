379fde372345dd32dd201d2c3318c3d048fbf9b6
HFV <hector.kaizen@gmail.com>
2022-01-14 13:09:31 -0600

    add study

--- javascript/wev-dev-simplified/callback-to-promise/04-my-try.js
+++ javascript/wev-dev-simplified/callback-to-promise/04-my-try.js
@@ -6,3 +6,15 @@ const task03 = () => new Promise(resolve => resolve('L'))
 const task04 = () => new Promise(resolve => resolve('L'))
 const task05 = () => new Promise(resolve => resolve('O'))
 
+
+const sayHello = () => {
+  return new Promise.all([
+    task01,
+    task02,
+    task03,
+    task04,
+    task05,
+  ])
+}
+
+sayHello()
