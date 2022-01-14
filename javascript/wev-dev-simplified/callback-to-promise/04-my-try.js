deaf29ec2fadabdade6410e648eca3d3e3bc1d57
HFV <hector.kaizen@gmail.com>
2022-01-14 13:07:15 -0600

    add study

---
+++ javascript/wev-dev-simplified/callback-to-promise/04-my-try.js
@@ -0,0 +1,8 @@
+
+
+const task01 = () => new Promise(resolve => resolve('H'))
+const task02 = () => new Promise(resolve => resolve('E'))
+const task03 = () => new Promise(resolve => resolve('L'))
+const task04 = () => new Promise(resolve => resolve('L'))
+const task05 = () => new Promise(resolve => resolve('O'))
+
